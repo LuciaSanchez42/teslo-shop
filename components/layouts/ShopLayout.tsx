73921b84cfa47e9016ed928aff9f8837ff46c19e
Crissthian <mediocero007.12@gmail.com>
2022-11-29 18:54:51 -0500

    estructura del directorio

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import { FunctionComponent, ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+  title: string
+  pageDescription: string
+  imageUrl?: string
+}
+
+const ShopLayout: FunctionComponent<Props> = ({ children, title, pageDescription, imageUrl }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name='og:title' content='initial-scale=1.0, width=device-width' />
+        <meta name='og:description' content={pageDescription} />
+        {imageUrl && <meta name='og:image' content={imageUrl} />}
+      </Head>
+      <nav>{/* TODO: navbar */}</nav>
+      {/* TODO: sidebar */}
+      <main
+        style={{
+          margin: '80px auto',
+          maxWidth: '1440px',
+          padding: '0 30px'
+        }}>
+        {children}
+      </main>
+    </>
+  )
+}
+
+export default ShopLayout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,16 @@
-const index = () => {
-  return <h1>home</h1>
+import { Typography } from '@mui/material'
+import { ShopLayout } from '../components/layouts'
+
+const Home = () => {
+  return (
+    <ShopLayout title='Teslo shop - home' pageDescription='encuentra los mejores productos'>
+      <Typography variant='h1' component={'h1'}>
+        Tienda
+      </Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
+  )
 }
-export default index
+export default Home
