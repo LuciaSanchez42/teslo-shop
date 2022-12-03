e270762438157c07a6c5ee636bd56e567f68faa7
Crissthian <mediocero007.12@gmail.com>
2022-12-02 21:36:53 -0500

    creando un modelo

---
+++ models/Product.ts
@@ -0,0 +1,66 @@
+import mongoose, { model, Model, Schema } from 'mongoose'
+import { SeedProduct } from '../database/products'
+
+const ProductSchema = new Schema<SeedProduct>(
+  {
+    description: {
+      type: String,
+      required: true
+    },
+    images: {
+      type: [String],
+      required: true
+    },
+    inStock: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    sizes: [
+      {
+        type: String,
+        enum: {
+          values: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+          message: '{VALUE} is not a valid size'
+        }
+      }
+    ],
+    slug: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    tags: {
+      type: [String]
+    },
+
+    title: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      enum: {
+        values: ['shirts', 'pants', 'hoodies', 'hats'],
+        message: '{VALUE} is not a valid type '
+      }
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ['men', 'women', 'kid', 'unisex'],
+        message: '{{value}} is not a gener valid'
+      }
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+const Product = mongoose.models.Product || model('Product', ProductSchema)
+export default Product
